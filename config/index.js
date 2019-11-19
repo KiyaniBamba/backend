63c6d8da1fe04915492726f953e41377b2e44544
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-11-19 19:27:50 +0000

    update

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const port = process.env.PORT || 4400;
-const dbUrl = process.env.DB_URL;
+const dbUrl = process.env.DATABASE_URL;
 const env = process.env.NODE_ENV || "development";
 const round = process.env.SALT_ROUND || 11;
 

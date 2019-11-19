67fa2659d23adc7522151e45f3c71dae0dc64191
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-11-19 09:05:01 +0000

    generateToken util

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken");
+
 const createError = (message, status = 500) => {
   const error = new Error(message);
   error.status(status);
@@ -5,6 +7,15 @@ const createError = (message, status = 500) => {
   return error;
 };
 
+const generateToken = user => {
+  const { id, username } = user;
+  const payload = { id, username };
+  const secret = process.env.JWT_SECRET || "secret";
+  const token = jwt.sign(payload, secret);
+  return token;
+};
+
 module.exports = {
-  createError
+  createError,
+  generateToken
 };
